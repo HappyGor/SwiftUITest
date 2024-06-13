d0d2bead87e7e5bf5a7a2bf9060b356d01ae242a
GorCat <>
2024-06-13 15:19:41 +0800

    dev: 空白项目

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by GorCat on 2024/6/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
