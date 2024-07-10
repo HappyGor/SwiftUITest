13ee988c51b2448294b64f2f6dd9bdf0056ea7ca
GorCat <>
2024-07-10 15:06:52 +0800

    Interfacing with UIKit

--- SwiftUITest/Landmarks/Model/LandmarksModel.swift
+++ SwiftUITest/Landmarks/Model/LandmarksModel.swift
@@ -31,6 +31,9 @@ struct LandmarksModel: Hashable, Codable, Identifiable {
     var image: Image {
         Image(imageName)
     }
+    var featureImage: Image? {
+        isFeatured ? Image(imageName + "_feature") : nil
+    }
     
     
     private var coordinates: Coordinates
